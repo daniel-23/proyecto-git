a976d5c6cef2b4e0b6c80101e3eac5b778f3705e
Daniel <danielbatlet@gmail.com>
2019-12-04 13:54:05 -0500

    version 0.0.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso de Git</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<h1>Esto es de prueba GIT</h1>
+	<script type="text/javascript" src="app.js"></script>
+</body>
+</html>
